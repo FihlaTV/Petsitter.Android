90939a61b07edffcde5e635809e2cfea4b6321c5
Katsutoshi Rech Fukuoka <katsutoshifr@gmail.com>
2017-04-20 22:44:22 -0300

    mListView + Firebase Retrieve, Create, + menu toolbar(incomplete)

--- PetSitter/app/src/main/java/com/example/katsutoshi/petsitter/HomeActivity.java
+++ PetSitter/app/src/main/java/com/example/katsutoshi/petsitter/HomeActivity.java
@@ -1,14 +1,20 @@
-package com.example.katsutoshi.petsitter.activity;
+package com.example.katsutoshi.petsitter;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.Menu;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +22,7 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.example.katsutoshi.petsitter.adapter.ListViewAdapter;
 import com.example.katsutoshi.petsitter.R;
@@ -31,14 +38,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
+    private String child = "pets/dogs";
     private ListView listPets;
     private ProgressBar circularProgressBar;
 
     private FirebaseDatabase mFirebaseDB;
     private DatabaseReference mDBReference;
-
+    private FloatingActionButton btnAdd;
     private Pet selectedPet;
 
     private List<Pet> pets = new ArrayList<>();
@@ -46,14 +55,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //add Toolbar
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.petToolbar);
-        //setSupportActionBar(toolbar);
-
-        //Control
-        final FloatingActionButton btnAdd = (FloatingActionButton) findViewById(R.id.fab);
         circularProgressBar = (ProgressBar) findViewById(R.id.circularProgressBar);
         listPets = (ListView) findViewById(R.id.listPets);
 
@@ -61,41 +66,31 @@ public class MainActivity extends AppCompatActivity {
         initFirebase();
         addEventFirebaseListener();
 
-        //New Pet Dialog
+        btnAdd = (FloatingActionButton) findViewById(R.id.fab);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //btnAdd.setVisibility(View.INVISIBLE);
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-                View mView = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_new_pet, null);
-                final EditText mPetName = (EditText) mView.findViewById(R.id.etPetName);
-                final EditText mPetBirth = (EditText) mView.findViewById(R.id.etPetBirth);
-                final EditText mPetWeight = (EditText) mView.findViewById(R.id.etPetWeight);
-                final Button mbtnAdd = (Button) mView.findViewById(R.id.btnPetAdd);
-                //View mView = getLayoutInflater().inflate(R.layout.dialog_new_pet, null);
-                mBuilder.setView(mView);
-                AlertDialog alert = mBuilder.create();
-                alert.show();
-                mbtnAdd.setOnClickListener(new View.OnClickListener() {
-                                               @Override
-                                               public void onClick(View view) {
-                                                   createPet(mPetName.getText(), mPetBirth.getText(), mPetWeight.getText());
-                                                   //btnAdd.setVisibility(View.VISIBLE);
-                                               }
-                                           }
-                );
-                mPetBirth.setText("");
-                mPetName.setText("");
-                mPetWeight.setText("");
+                AddPetAction();
             }
         });
-   }
+
+        //btnAdd.setVisibility(View.INVISIBLE);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
     private void addEventFirebaseListener() {
         circularProgressBar.setVisibility(View.VISIBLE);
         listPets.setVisibility(View.INVISIBLE);
 
-        mDBReference.child("pets").addValueEventListener(new ValueEventListener() {
+        mDBReference.child(this.child).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 pets.clear();
@@ -103,9 +98,8 @@ public class MainActivity extends AppCompatActivity {
                     Pet pet = postSnapshot.getValue(Pet.class);
                     pets.add(pet);
                 }
-                ListViewAdapter adapter = new ListViewAdapter(MainActivity.this, pets);
+                ListViewAdapter adapter = new ListViewAdapter(HomeActivity.this, pets);
                 listPets.setAdapter(adapter);
-                circularProgressBar.setVisibility(View.INVISIBLE);
                 listPets.setVisibility(View.VISIBLE);
             }
             @Override
@@ -116,9 +110,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Pet lstPet = (Pet)parent.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, lstPet.getName() + "Click", Toast.LENGTH_SHORT).show();
+                Toast.makeText(HomeActivity.this, lstPet.getName() + "Click", Toast.LENGTH_SHORT).show();
             }
         });
+
+        circularProgressBar.setVisibility(View.INVISIBLE);
     }
 
     private void initFirebase() {
@@ -129,39 +125,37 @@ public class MainActivity extends AppCompatActivity {
         mDBReference.keepSynced(true);
     }
 
-    private android.widget.AdapterView.OnItemClickListener CallItemFunction()
-    {
-        return (new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                listPets.getSelectedItem();
-            }
-        });
+    private void AddPetAction() {
+        //btnAdd.setVisibility(View.INVISIBLE);
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(HomeActivity.this);
+        View mView = LayoutInflater.from(HomeActivity.this).inflate(R.layout.dialog_new_pet, null);
+        final EditText mPetName = (EditText) mView.findViewById(R.id.etPetName);
+        final EditText mPetBirth = (EditText) mView.findViewById(R.id.etPetBirth);
+        final EditText mPetWeight = (EditText) mView.findViewById(R.id.etPetWeight);
+        final Button mbtnAdd = (Button) mView.findViewById(R.id.btnPetAdd);
+        //View mView = getLayoutInflater().inflate(R.layout.dialog_new_pet, null);
+        mBuilder.setView(mView);
+        AlertDialog alert = mBuilder.create();
+        alert.show();
+        mbtnAdd.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View view) {
+                                           createPet(mPetName.getText(), mPetBirth.getText(), mPetWeight.getText());
+                                           //btnAdd.setVisibility(View.VISIBLE);
+                                       }
+                                   }
+        );
+        mPetBirth.setText("");
+        mPetName.setText("");
+        mPetWeight.setText("");
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    /*
-     @Override
-     public boolean onOptionsItemSelected(MenuItem item)
-     {
-        if(item.getItemId() == R.id.btnPetAdd)
-        {
-            //createPet();
-        }
-     }
-     */
 
     private void createPet(Editable name, Editable birth, Editable weight) {
 
         if(validate(name, birth, weight)) {
             Pet pet = new Pet(UUID.randomUUID().toString(), name.toString(), Double.parseDouble(weight.toString()), birth.toString());
-            mDBReference.child("pets").child(pet.getUid()).setValue(pet);
-            Toast.makeText(MainActivity.this, name.toString() + " cadastrado", Toast.LENGTH_LONG).show();
+            mDBReference.child(child).child(pet.getUid()).setValue(pet);
+            Toast.makeText(HomeActivity.this, name.toString() + " cadastrado", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -195,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
                 alert.show();
                 return false;
             }
-            //validate data types 
+            //validate data types
             //Verify max weight
             if (Integer.parseInt(weight.toString()) <= 0 || Integer.parseInt(weight.toString()) > 240) {
                 msg.setText("Peso inválido.");
@@ -214,5 +208,57 @@ public class MainActivity extends AppCompatActivity {
             alert.show();
             return false;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_dog) {
+            this.child = "pets/dogs";
+            btnAdd.setVisibility(View.VISIBLE);
+            addEventFirebaseListener();
+        } else if (id == R.id.nav_cat) {
+            this.child = "pets/cats";
+            btnAdd.setVisibility(View.VISIBLE);
+            addEventFirebaseListener();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
